b88cd6fee76441e9a2b82b05e1f87c84817d9237
sshahary <145033155+sshahary@users.noreply.github.com>
2024-09-03 16:45:22 +0200

    init wp script

---
+++ srcs/requirements/wordpress/tools/script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd /var/www/html
+
+rm -rf *
+
+wp core download --allow-root
+
+sleep 5
+
+wp config create	--allow-root --force \
+					--url="$WP_URL" \
+					--dbname="$DB_NAME" \
+					--dbuser="$DB_USER" \
+					--dbpass="$DB_PASSWORD" \
+					--dbhost="mariadb:3306"
+
+wp core install	--allow-root --url="$WP_URL" \
+				--title="$WP_TITLE" \
+				--admin_user="$WP_ADMIN_NAME" \
+				--admin_password="$WP_ADMIN_PASS" \
+				--admin_email="$WP_ADMIN_EMAIL" \
+				--skip-email
+
+wp user create	--allow-root $WP_USER_NAME \
+				$WP_USER_EMAIL \
+				--user_pass="$WP_USER_PASS"
+
+php-fpm7.3 -F
