db45979f25887085e970c1faf9cf097c1fd6b4e6
sshahary <145033155+sshahary@users.noreply.github.com>
2024-09-05 13:33:48 +0200

    Changed variable names

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -10,16 +10,16 @@ sleep 5
 
 wp config create	--allow-root --force \
 					--url="$WP_URL" \
-					--dbname="$DB_NAME" \
-					--dbuser="$DB_USER" \
-					--dbpass="$DB_PASSWORD" \
+					--dbname="$SQL_DATABASE" \
+					--dbuser="$SQL_USER" \
+					--dbpass="$SQL_PASSWORD" \
 					--dbhost="mariadb:3306"
 
 wp core install	--allow-root --url="$WP_URL" \
 				--title="$WP_TITLE" \
-				--admin_user="$WP_ADMIN_NAME" \
+				--admin_user="$WP_ADMIN" \
 				--admin_password="$WP_ADMIN_PASS" \
-				--admin_email="$WP_ADMIN_EMAIL" \
+				--admin_email="$WP_ADMIN_MAIL" \
 				--skip-email
 
 wp user create	--allow-root $WP_USER_NAME \
